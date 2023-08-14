34fb2dc1523170bfde87178b9f884f1ff10b7c7c
Вера Фауст <faust.vera@yandex.ru>
2023-08-14 22:48:14 +0300

    deploy14

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.250.22.151', '127.0.0.1', 'cats.sytes.net']
+ALLOWED_HOSTS = ['cats.sytes.net', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
